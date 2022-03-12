83343ab4cd8940bf6e4b6684da19d33c48401f64
rustgen <nrustgen@gmail.com>
2022-03-11 19:13:23 -0800

    6. Checkstyle [#4123]

--- job4j_elementary/src/main/java/ru/job4j/StudentInfo.java
+++ job4j_elementary/src/main/java/ru/job4j/StudentInfo.java
@@ -1,6 +1,6 @@
 package ru.job4j;
 
-public class studentInfo {
+public class StudentInfo {
     public static void main(String[] args) {
         System.out.println("Rustem");
         System.out.println("01.01.1991");
