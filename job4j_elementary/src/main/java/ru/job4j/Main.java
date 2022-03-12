765763c47e49d8f2e1bae1cee15c917ff27c9247
rustgen <nrustgen@gmail.com>
2022-03-12 13:21:33 -0800

    6. Checkstyle [#4123]

--- job4j_elementary/src/main/java/ru/job4j/Main.java
+++ job4j_elementary/src/main/java/ru/job4j/Main.java
@@ -1,6 +1,7 @@
 package ru.job4j;
 
 public class Main {
+    @SuppressWarnings("checkstyle:LeftCurly")
     public static void main(String[] args) {
         System.out.println("Hello world!");
     }
