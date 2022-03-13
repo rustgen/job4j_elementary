d72a5768de253f58f606935c1e92467be20c2678
rustgen <nrustgen@gmail.com>
2022-03-12 16:58:08 -0800

    3. Арифметические операции. [#185]

--- job4j_elementary/src/main/java/ru/job4j/Main.java
+++ job4j_elementary/src/main/java/ru/job4j/Main.java
@@ -1,7 +1,7 @@
 package ru.job4j;
 
 public class Main {
-    @SuppressWarnings("checkstyle:LeftCurly")
+    @SuppressWarnings({"checkstyle:LeftCurly", "checkstyle:EmptyLineSeparator"})
     public static void main(String[] args) {
         System.out.println("Hello world!");
     }
