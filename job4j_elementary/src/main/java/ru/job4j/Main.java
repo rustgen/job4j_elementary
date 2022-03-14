2b5c33c95ec03021d2a4d9b78967bc77969ec018
rustgen <nrustgen@gmail.com>
2022-03-14 01:07:45 -0700

    new

--- job4j_elementary/src/main/java/ru/job4j/Main.java
+++
@@ -1,8 +0,0 @@
-package ru.job4j;
-
-public class Main {
-    @SuppressWarnings({"checkstyle:LeftCurly", "checkstyle:EmptyLineSeparator"})
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
-    }
-}
