070435511999df958262f55fea027658826a5bea
rustgen <nrustgen@gmail.com>
2022-03-14 01:04:00 -0700

    first commit

---
+++ src/main/java/ru/job4j/Main.java
@@ -0,0 +1,8 @@
+package ru.job4j;
+
+public class Main {
+    @SuppressWarnings({"checkstyle:LeftCurly", "checkstyle:EmptyLineSeparator"})
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
