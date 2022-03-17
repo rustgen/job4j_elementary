6c4db74de9f645d368632a445acd43c90fc56d91
rustgen <nrustgen@gmail.com>
2022-03-17 00:35:09 -0700

    4.1.2. Глупый бот [#31323]

--- src/main/java/ru/job4j/condition/Game.java
+++ src/main/java/ru/job4j/condition/Game.java
@@ -1,7 +1,7 @@
 package ru.job4j.condition;
 
 public class Game {
-    public static void menu (String name) {
+    public static void menu(String name) {
         if ("super mario".equals(name)) {
             System.out.println("Start - super mario");
         }
