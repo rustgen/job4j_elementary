f91ead2359a3ceca825845421e4821a3d4649437
rustgen <nrustgen@gmail.com>
2022-03-19 22:25:03 -0700

    5.0. Цикл for. Определение. [#3572]

---
+++ src/main/java/ru/job4j/loop/LoopFor.java
@@ -0,0 +1,9 @@
+package ru.job4j.loop;
+
+public class LoopFor {
+    public static void main(String[] args) {
+        for (int num = 5; num <= 10; num++) {
+            System.out.println(num);
+        }
+    }
+}
