50e398e3e7e97695bd3e4a68a2f7f4493f90af8a
rustgen <nrustgen@gmail.com>
2022-03-22 00:17:45 -0700

    5.4. Протеиновая диета [#156313 #278173]

--- src/main/java/ru/job4j/loop/Fitness.java
+++ src/main/java/ru/job4j/loop/Fitness.java
@@ -4,16 +4,9 @@ public class Fitness {
 
     public static int calc(int ivan, int nik) {
         int month = 0;
-        int thrustI = 0;
-        int thrustN = 0;
-        if (ivan > nik) {
-            month--;
-        }
-        while (thrustI <= thrustN) {
+        while (ivan <= nik) {
             ivan *= 3;
             nik *= 2;
-            thrustI = ivan;
-            thrustN = nik;
             month++;
         }
 
@@ -22,6 +15,6 @@ public class Fitness {
 
     public static void main(String[] args) {
 //        calc(95, 90);
-        System.out.println(calc(95, 90));
+        System.out.println(calc(50, 90));
     }
 }
