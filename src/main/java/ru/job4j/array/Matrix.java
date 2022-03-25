f6fbd1920c2b6b4150ec832b5ed9559f0c0ae983
rustgen <nrustgen@gmail.com>
2022-03-25 01:15:50 -0700

    6.5.3. Сортировка выборкой. [#156320 #280061]

--- src/main/java/ru/job4j/array/Matrix.java
+++ src/main/java/ru/job4j/array/Matrix.java
@@ -3,9 +3,8 @@ package ru.job4j.array;
 public class Matrix {
     public static int[][] multiple(int size) {
         int[][] value = new int[size][size];
-        for (int row = 0; row < size; row++) {
-            System.out.println();
-            for (int cell = 0; cell < size; cell++) {
+        for (int row = 0; row < value.length; row++) {
+            for (int cell = 0; cell < value[row].length; cell++) {
                 System.out.println();
                 value[row][cell] = (row + 1) * (cell + 1);
             }
--- src/test/java/ru/job4j/array/SortSelectedTest.java
+++ src/test/java/ru/job4j/array/SortSelectedTest.java
@@ -14,4 +14,20 @@ public class SortSelectedTest {
         int[] expected = new int[] {1, 2, 3, 4, 5};
         Assert.assertArrayEquals(expected, result);
     }
+
+    @Test
+    public void whenSortArrayOf7elements() {
+        int[] data = new int[] {31, 42, 144, 23, 52, 56, 50};
+        int[] result = SortSelected.sort(data);
+        int[] expected = new int[] {23, 31, 42, 50, 52, 56, 144};
+        Assert.assertArrayEquals(expected, result);
+    }
+
+    @Test
+    public void whenSortArrayOf3elements() {
+        int[] data = new int[] {22, 5, 53};
+        int[] result = SortSelected.sort(data);
+        int[] expected = new int[] {5, 22, 53};
+        Assert.assertArrayEquals(expected, result);
+    }
 }
