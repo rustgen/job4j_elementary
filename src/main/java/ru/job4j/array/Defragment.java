f900eeddbbb1b6677915011fb69407e790029a80
rustgen <nrustgen@gmail.com>
2022-03-26 14:09:29 -0700

    6.7.4. Выигрышные комбинации в сокобан [#53859 #280491]

---
+++ src/main/java/ru/job4j/array/Defragment.java
@@ -0,0 +1,32 @@
+package ru.job4j.array;
+
+public class Defragment {
+    public static String[] compress(String[] array) {
+        for (int index = 0; index < array.length; index++) {
+            if (array[index] == null) {
+                for (int i = index; i < array.length - i; i++) {
+                    if (array[index + i] != null) {
+                        String temp = array[index + i];
+                        array[index + i] = array[index];
+                        array[index] = temp;
+                        break;
+                    }
+                }
+            }
+        }
+        return array;
+    }
+
+    public static void main(String[] args) {
+        String[] input = {null, null, null, "I", "wanna", null,"be", null, null, "compressed"};
+        //String[] input = {null, "I"};
+        //String[] input = {"I", null, "wanna", null, "be"};
+        //String[] input = {"I", null, "wanna", null, "be", null, "compressed"};
+        //String[] input = {null, "I", "wanna", null, "be", null, "compressed"};
+        String[] compressed = compress(input);
+        System.out.println();
+        for (int index = 0; index < compressed.length; index++) {
+            System.out.print(compressed[index] + " ");
+        }
+    }
+}
