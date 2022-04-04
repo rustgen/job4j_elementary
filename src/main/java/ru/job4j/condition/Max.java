5c62c34e23b248ec1a27bd7f5d4e3b5f22a6de63
rustgen <nrustgen@gmail.com>
2022-04-04 03:06:08 -0700

    3. Перегрузить метод max для трех чисел [#122644]

--- src/main/java/ru/job4j/condition/Max.java
+++ src/main/java/ru/job4j/condition/Max.java
@@ -7,6 +7,20 @@ public class Max {
         return result;
     }
 
+    public static int max(int left, int middle, int right) {
+        return max(
+                left,
+                max(middle, right)
+        );
+    }
+
+    public static int max(int left1, int left2, int right1, int right2) {
+        return max(
+                max(left1, left2),
+                max(right1, right2)
+        );
+    }
+
     public static void main(String[] args) {
         int num = Max.max(2, 11);
         System.out.println(num);
--- src/test/java/ru/job4j/condition/MaxTest.java
+++ src/test/java/ru/job4j/condition/MaxTest.java
@@ -33,4 +33,46 @@ public class MaxTest {
         int expected = 2;
         Assert.assertEquals(expected, result);
     }
+
+    @Test
+    public void whenMax2To2To3Then3() {
+        int left = 2;
+        int middle = 2;
+        int right = 3;
+        int result = Max.max(left, Max.max(middle, right));
+        int expected = 3;
+        Assert.assertEquals(expected, result);
+    }
+
+    @Test
+    public void whenMax21To2To3Then21() {
+        int left = 21;
+        int middle = 2;
+        int right = 3;
+        int result = Max.max(left, Max.max(middle, right));
+        int expected = 21;
+        Assert.assertEquals(expected, result);
+    }
+
+    @Test
+    public void whenMax21To21To3To3Then21() {
+        int left1 = 21;
+        int left2 = 21;
+        int right1 = 3;
+        int right2 = 3;
+        int result = Max.max(Max.max(left1, left2), Max.max(right1, right2));
+        int expected = 21;
+        Assert.assertEquals(expected, result);
+    }
+
+    @Test
+    public void whenMax1To7To9To4Then9() {
+        int left1 = 1;
+        int left2 = 7;
+        int right1 = 9;
+        int right2 = 4;
+        int result = Max.max(Max.max(left1, left2), Max.max(right1, right2));
+        int expected = 9;
+        Assert.assertEquals(expected, result);
+    }
 }
