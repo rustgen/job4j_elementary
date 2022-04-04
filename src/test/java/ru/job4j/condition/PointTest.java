edca4e26f08409bbfa254c09d34b7ac470f221e5
rustgen <nrustgen@gmail.com>
2022-04-03 19:21:46 -0700

    2.1. Рефакторинг - Расстояние между точками. [#122587 #283726]

--- src/test/java/ru/job4j/condition/PointTest.java
+++ src/test/java/ru/job4j/condition/PointTest.java
@@ -12,7 +12,6 @@ public class PointTest {
     public void whenThis00That20ThenDistanceEqual20() {
         Point a = new Point(0, 0);
         Point b = new Point(2, 0);
-        a.distance(b);
         double expected = 2.0;
         assertThat(expected, is(a.distance(b)));
     }
@@ -21,7 +20,6 @@ public class PointTest {
     public void whenThis01That21ThenDistanceEqual20() {
         Point a = new Point(0, 1);
         Point b = new Point(2, 1);
-        a.distance(b);
         double expected = 2.0;
         assertThat(expected, is(a.distance(b)));
     }
@@ -30,7 +28,6 @@ public class PointTest {
     public void whenThis21That21ThenDistanceEqual0() {
         Point a = new Point(2, 1);
         Point b = new Point(2, 1);
-        a.distance(b);
         double expected = 0;
         assertThat(expected, is(a.distance(b)));
     }
@@ -39,7 +36,6 @@ public class PointTest {
     public void whenThis23That25ThenDistanceEqual20() {
         Point a = new Point(2, 3);
         Point b = new Point(2, 5);
-        a.distance(b);
         double expected = 2.0;
         assertThat(expected, is(a.distance(b)));
     }
